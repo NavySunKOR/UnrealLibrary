00b7e6f08443c68db52c96b2ccdb22044585caf3
NavySunKOR <dragunov2008@naver.com>
2023-06-03 19:20:27 +0900

    ProceduralGenerating 관련 백업 업로드. 추후 플러그인으로 변경 예정.

---
+++ ProceduralLandscape/Source/ProceduralGenerator/NoiseMapGenerator.cpp
@@ -0,0 +1,96 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ProceduralGenerator/NoiseMapGenerator.h"
+#include "Kismet/KismetMathLibrary.h"
+
+// Sets default values
+ANoiseMapGenerator::ANoiseMapGenerator()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	SizeX = 128;
+	SizeY = 128;
+
+}
+
+// Called when the game starts or when spawned
+void ANoiseMapGenerator::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ANoiseMapGenerator::GenerateNoiseMap()
+{
+	GenerateNoiseData();
+	GenerateTexture();
+
+}
+
+
+void ANoiseMapGenerator::GenerateNoiseData()
+{
+	if (NoiseData)
+	{
+		delete NoiseData;
+		NoiseData = nullptr;
+	}
+
+	NoiseData = new float[SizeX * SizeY];
+
+	for (int32 YPos = 0; YPos < SizeY; ++YPos)
+	{
+		for (int32 XPos = 0; XPos < SizeX; ++XPos)
+		{
+			float AccFrequency = 1;
+			float AccAmplitude = 1;
+			float MaxValue = 0;
+			float Sumup = 0;
+			for (int32 Octa = 0; Octa < Octave; ++Octa)
+			{
+				float XPosF = ((float)XPos / (float)SizeX + OffsetX) * AccFrequency;
+				float YPosF = ((float)YPos / (float)SizeY + OffsetY) * AccFrequency;
+
+				Sumup += FMath::PerlinNoise2D(FVector2D(XPosF, YPosF)) * AccAmplitude;
+
+				MaxValue += AccAmplitude;
+				AccAmplitude *= 2;
+				AccFrequency *= Persistence;
+			}
+
+			float Result = Sumup / MaxValue;
+			Result = (Result + 1.f) / 2.f;
+
+			NoiseData[XPos + YPos * SizeX] = Result;
+		}
+	}
+
+}
+
+
+void ANoiseMapGenerator::GenerateTexture()
+{
+	if (GeneratedTexture == nullptr)
+	{
+		GeneratedTexture = UTexture2D::CreateTransient(SizeX, SizeY, PF_B8G8R8A8);
+	}
+
+
+	FColor* Color = (FColor*)(GeneratedTexture->PlatformData->Mips[0].BulkData.Lock(LOCK_READ_WRITE));
+	for (int32 YPos = 0; YPos < SizeY; ++YPos)
+	{
+		for (int32 XPos = 0; XPos < SizeX; ++XPos)
+		{
+			int32 Idx = XPos + YPos * SizeX;
+			Color[Idx].R = NoiseData[Idx] * 255;
+			Color[Idx].G = NoiseData[Idx] * 255;
+			Color[Idx].B = NoiseData[Idx] * 255;
+			Color[Idx].A = 255;
+		}
+	}
+
+	GeneratedTexture->PlatformData->Mips[0].BulkData.Unlock();
+	GeneratedTexture->UpdateResource();
+}
---
+++ ProceduralLandscape/Source/ProceduralGenerator/NoiseMapGenerator.h
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "NoiseMapGenerator.generated.h"
+
+UCLASS()
+class UE5PROGRAMMINGDEV_API ANoiseMapGenerator : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ANoiseMapGenerator();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Noise Map Setting")
+	int32 SizeX;
+
+	UPROPERTY(EditAnywhere, Category = "Noise Map Setting")
+	int32 SizeY;
+
+	UPROPERTY(EditAnywhere, Category = "Noise Map Setting")
+	int32 OffsetX;
+
+	UPROPERTY(EditAnywhere, Category = "Noise Map Setting")
+	int32 OffsetY;
+
+	UPROPERTY(EditAnywhere, Category = "Noise Map Setting")
+	int32 Octave;
+
+	UPROPERTY(EditAnywhere, Category = "Noise Map Setting")
+	float Persistence;
+
+
+
+	UPROPERTY(VisibleAnywhere)
+	UTexture2D* GeneratedTexture = nullptr;
+
+
+#if WITH_EDITOR 
+
+	UFUNCTION(CallInEditor)
+	void GenerateNoiseMap();
+
+#endif
+
+protected: 
+	//Range∏¶ 0~1∑Œ InverseLerp «ÿ¡‹.
+	float* NoiseData = nullptr;
+	void GenerateNoiseData();
+
+	void GenerateTexture();
+
+
+
+};
